1140cbdf4f1a501e717056db030a0337b3014d80
idiotf <aqu3180@naver.com>
2025-05-20 17:27:28 +0900

    deleted:    blank.png 	modified:   bun.lock 	modified:   package.json 	modified:   src/client/index.ts 	modified:   src/server/index.ts

--- src/client/index.ts
+++ src/client/index.ts
@@ -5,10 +5,10 @@ import type { Client2Server, Server2Client, ChunkName } from '../server'
 const canvas = document.querySelector('canvas') as HTMLCanvasElement
 const context = canvas.getContext('2d')!
 
-function rerender() {
+function rerender(clear = true) {
   context.imageSmoothingEnabled = false
   context.resetTransform()
-  context.clearRect(0, 0, canvas.width, canvas.height)
+  if (clear) context.clearRect(0, 0, canvas.width, canvas.height)
   context.setTransform(camera.zoom, 0, 0, camera.zoom, Math.round(canvas.width / 2 + camera.x * camera.zoom), Math.round(canvas.height / 2 + camera.y * camera.zoom))
 
   const chunkX1 = Math.floor((-camera.x * camera.zoom - canvas.width / 2) / camera.zoom / CHUNK_SIZE)
@@ -17,12 +17,10 @@ function rerender() {
   const chunkY2 = Math.ceil((-camera.y * camera.zoom + canvas.height / 2) / camera.zoom / CHUNK_SIZE)
 
   const promises = []
-  for (let chunkY = chunkY1; chunkY < chunkY2; ++chunkY) for (let chunkX = chunkX1; chunkX < chunkX2; ++chunkX) promises.push((async () => {
-    // putImageContext.putImageData(new ImageData(await readChunk(BigInt(chunkX), BigInt(chunkY)), CHUNK_SIZE), 0, 0)
-    // context.drawImage(putImageCanvas, chunkX * CHUNK_SIZE, chunkY * CHUNK_SIZE)
-    const chunk = readChunk(BigInt(chunkX), BigInt(chunkY))
-    if (chunk.complete) context.drawImage(chunk, chunkX * CHUNK_SIZE, chunkY * CHUNK_SIZE)
-    else chunk.addEventListener('load', () => context.drawImage(chunk, chunkX * CHUNK_SIZE, chunkY * CHUNK_SIZE))
+  for (let chunkY = BigInt(chunkY1); chunkY < chunkY2; ++chunkY) for (let chunkX = BigInt(chunkX1); chunkX < chunkX2; ++chunkX) promises.push((async () => {
+    putImageContext.putImageData(new ImageData(await readChunk(BigInt(chunkX), BigInt(chunkY)), CHUNK_SIZE), 0, 0)
+    context.clearRect(Number(chunkX) * CHUNK_SIZE, Number(chunkY) * CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE)
+    context.drawImage(putImageCanvas, Number(chunkX) * CHUNK_SIZE, Number(chunkY) * CHUNK_SIZE)
   })())
 }
 
@@ -51,18 +49,22 @@ const drawOptions = {
   color: '#000',
 }
 
+canvas.style.cursor = 'crosshair'
 window.addEventListener('keydown', event => {
   switch (event.key) {
     case 'Enter':
       switch (drawOptions.mode) {
         case DrawMode.VIEW:
           drawOptions.mode = DrawMode.DRAW
+          canvas.style.cursor = 'crosshair'
           break
         case DrawMode.DRAW:
           drawOptions.mode = DrawMode.ERASE
+          canvas.style.cursor = 'crosshair'
           break
         case DrawMode.ERASE:
           drawOptions.mode = DrawMode.VIEW
+          canvas.style.cursor = 'move'
           break
       }
       break
@@ -75,42 +77,71 @@ window.addEventListener('keydown', event => {
   }
 })
 
-const getCoord = (x: number, width: number, cameraX: number) => Math.round((x * devicePixelRatio - width / 2) / camera.zoom - cameraX) + drawOptions.lineWidth % 2 / 2
+const getCoord = (x: number, width: number, cameraX: number) => Math.round((x * devicePixelRatio - width / 2) / camera.zoom - cameraX - drawOptions.lineWidth % 2 / 2) + drawOptions.lineWidth % 2 / 2
 
 canvas.addEventListener('mousedown', event => {
-  const lineAction = (event: MouseEvent, isErase = false) => drawLine(
-    oldX, oldY,
-    oldX = getCoord(event.clientX, canvas.width, camera.x), oldY = getCoord(event.clientY, canvas.height, camera.y),
-    drawOptions.lineWidth, drawOptions.color,
-    isErase,
-  )
-  let oldX = getCoord(event.clientX, canvas.width, camera.x), oldY = getCoord(event.clientY, canvas.height, camera.y)
+  switch (event.button) {
+    case 0: {
+      const lineAction = (event: MouseEvent, isErase = false) => drawLine(
+        oldX, oldY,
+        oldX = getCoord(event.clientX, canvas.width, camera.x), oldY = getCoord(event.clientY, canvas.height, camera.y),
+        drawOptions.lineWidth, drawOptions.color,
+        isErase,
+      )
+      let oldX = getCoord(event.clientX, canvas.width, camera.x), oldY = getCoord(event.clientY, canvas.height, camera.y)
 
-  function move(event: MouseEvent) {
-    switch (drawOptions.mode) {
-      case DrawMode.VIEW:
-        camera.x += event.movementX / camera.zoom
-        camera.y += event.movementY / camera.zoom
+      function move(event: MouseEvent) {
+        switch (drawOptions.mode) {
+          case DrawMode.VIEW:
+            camera.x += event.movementX / camera.zoom
+            camera.y += event.movementY / camera.zoom
+            rerender()
+            break
+          case DrawMode.DRAW:
+            lineAction(event)
+            break
+          case DrawMode.ERASE:
+            lineAction(event, true)
+            break
+        }
+      }
+
+      function stop() {
+        removeEventListener('mousemove', move)
+        removeEventListener('mouseup', stop)
+      }
+
+      addEventListener('mousemove', move)
+      addEventListener('mouseup', stop)
+
+      move(event)
+      break
+    }
+    case 1: {
+      let x = 0, y = 0
+
+      function move(moveEvent: MouseEvent) {
+        x -= moveEvent.movementX
+        y -= moveEvent.movementY
+      }
+
+      function stop() {
+        removeEventListener('mousemove', move)
+        removeEventListener('mouseup', stop)
+        clearInterval(interval)
+      }
+
+      const interval = setInterval(() => {
+        camera.x += x * 0.05 / camera.zoom
+        camera.y += y * 0.05 / camera.zoom
         rerender()
-        break
-      case DrawMode.DRAW:
-        lineAction(event)
-        break
-      case DrawMode.ERASE:
-        lineAction(event, true)
-        break
+      })
+
+      addEventListener('mousemove', move)
+      addEventListener('mouseup', stop)
+      break
     }
   }
-
-  function stop() {
-    removeEventListener('mousemove', move)
-    removeEventListener('mouseup', stop)
-  }
-
-  addEventListener('mousemove', move)
-  addEventListener('mouseup', stop)
-
-  move(event)
 })
 
 canvas.addEventListener('touchstart', event => {
@@ -148,102 +179,81 @@ canvas.addEventListener('touchstart', event => {
   addEventListener('touchend', stop)
 
   move(event)
-})
+}, { passive: true })
 
 canvas.addEventListener('wheel', event => {
-  const oldZoom = camera.zoom
   camera.zoom = Math.max(1, Math.min(camera.zoom * 2 ** -Math.sign(event.deltaY), 256))
-  camera.x += (event.clientX - canvas.width / 2 - camera.x) * camera.zoom / oldZoom
-  camera.y += (event.clientY - canvas.height / 2 - camera.y) * camera.zoom / oldZoom
   rerender()
-})
+}, { passive: true })
 
-class CompleteOffscreenCanvas extends OffscreenCanvas {
-  complete: true = true
-}
-
-const chunkCanvas = new CompleteOffscreenCanvas(CHUNK_SIZE, CHUNK_SIZE)
+const chunkCanvas = new OffscreenCanvas(CHUNK_SIZE, CHUNK_SIZE)
 const chunkContext = chunkCanvas.getContext('2d', { willReadFrequently: true })!
 chunkContext.lineCap = 'round'
 
 const putImageCanvas = new OffscreenCanvas(CHUNK_SIZE, CHUNK_SIZE)
-const putImageContext = putImageCanvas.getContext('2d')!
+const putImageContext = putImageCanvas.getContext('2d', { willReadFrequently: true })!
 
 const socket: Socket<Server2Client, Client2Server> = io()
-// const chunks: Record<ChunkName, Promise<Uint8ClampedArray>> = {}
-// socket.on('chunk', (strX, strY, data) => {
-//   const x = BigInt(strX), y = BigInt(strY)
-//   const name: ChunkName = `${x},${y}`
-//   chunks[name] = (async () => data = new Uint8ClampedArray(data))()
-
-//   context.clearRect(Number(x) * CHUNK_SIZE, Number(y) * CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE)
-//   putImageContext.putImageData(new ImageData(data, CHUNK_SIZE), 0, 0)
-//   context.drawImage(putImageCanvas, Number(x) * CHUNK_SIZE, Number(y) * CHUNK_SIZE)
-// })
-interface AbortableImage {
-  image: HTMLImageElement | CanvasImageSource & { complete: true }
-  controller?: AbortController
-}
-
-const chunks: Record<ChunkName, AbortableImage> = {}
-socket.on('chunk', (x, y) => {
-  const name: ChunkName = `${BigInt(x)}.${BigInt(y)}.png`
-  const image = new Image
-  const controller = new AbortController
-
-  const prevChunk: AbortableImage | undefined = chunks[name]
-  chunks[name] = { image, controller }
-
-  image.addEventListener('load', () => {
-    prevChunk?.controller?.abort()
-    context.clearRect(+x * CHUNK_SIZE, +y * CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE)
-    context.drawImage(image, +x * CHUNK_SIZE, +y * CHUNK_SIZE)
-  }, controller)
-  image.src = `/paint_chunks/${name}`
+setInterval(() => socket.emit('ping'), 1000)
+socket.on('connect', () => {
+  setRange(true)
+  for (const key in chunks) delete chunks[key as ChunkName]
+  rerender(false)
 })
 
-// async function readChunk<X extends bigint, Y extends bigint>(x: X, y: Y) {
-//   const name: ChunkName<X, Y> = `${x},${y}`
-//   const cachedChunk = await chunks[name]
-//   if (cachedChunk) return cachedChunk
+const chunks: Record<ChunkName, Promise<Uint8ClampedArray>> = Object.create(null)
+const lastModifiedTable: Record<ChunkName, number> = Object.create(null)
+socket.on('chunk', (strX, strY, data) => {
+  const x = BigInt(strX), y = BigInt(strY)
+  const name: ChunkName = `${x},${y}`
+  chunks[name] = (async () => data = new Uint8ClampedArray(data))()
 
-//   try {
-//     return await (chunks[name] = (async () => new Uint8ClampedArray(await socket.emitWithAck('readChunk', x + '', y + '')))())
-//   } catch {
-//     return chunks[name] = (async () => new Uint8ClampedArray(chunkByteLength))()
-//   }
-// }
+  putImageContext.putImageData(new ImageData(data, CHUNK_SIZE), 0, 0)
+  context.clearRect(+strX * CHUNK_SIZE, +strY * CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE)
+  context.drawImage(putImageCanvas, +strX * CHUNK_SIZE, +strY * CHUNK_SIZE)
+})
 
-// async function writeChunk<X extends bigint, Y extends bigint>(x: X, y: Y, data: Uint8ClampedArray) {
-//   if (data.length != chunkByteLength) throw new RangeError(`Chunk size is not matching (Expected ${chunkByteLength}, Received ${data.length})`)
-
-//   const name: ChunkName<X, Y> = `${x},${y}`
-//   chunks[name] = (async () => data)()
-
-//   socket.emit('writeChunk', x + '', y + '', data)
-// }
-
-function readChunk<X extends bigint, Y extends bigint>(x: X, y: Y) {
-  const name: ChunkName<X, Y> = `${x}.${y}.png`
-  const chunk = chunks[name]
-  if (chunk) return chunk.image
-
-  const image = new Image
-  const controller = new AbortController
-  chunks[name] = { image, controller }
-
-  image.src = `/paint_chunks/${name}`
-  return image
+let prevChunkX1: number, prevChunkY1: number, prevChunkX2: number, prevChunkY2: number
+function setRange(force = false) {
+  const chunkX1 = Math.floor((-camera.x * camera.zoom - canvas.width / 2) / camera.zoom / CHUNK_SIZE)
+  const chunkY1 = Math.floor((-camera.y * camera.zoom - canvas.height / 2) / camera.zoom / CHUNK_SIZE)
+  const chunkX2 = Math.ceil((-camera.x * camera.zoom + canvas.width / 2) / camera.zoom / CHUNK_SIZE)
+  const chunkY2 = Math.ceil((-camera.y * camera.zoom + canvas.height / 2) / camera.zoom / CHUNK_SIZE)
+  if (force || chunkX1 != prevChunkX1 || chunkY1 != prevChunkY1 || chunkX2 != prevChunkX2 || chunkY2 != prevChunkY2) socket.emit('setRange', (prevChunkX1 = chunkX1) + '', (prevChunkY1 = chunkY1) + '', (prevChunkX2 = chunkX2) + '', (prevChunkY2 = chunkY2) + '')
 }
 
-async function writeChunk<X extends bigint, Y extends bigint>(x: X, y: Y, image: CompleteOffscreenCanvas) {
-  const name: ChunkName<X, Y> = `${x}.${y}.png`
-  chunks[name] = { image }
+async function readChunk<X extends bigint, Y extends bigint>(x: X, y: Y) {
+  setRange()
+  const name: ChunkName<X, Y> = `${x},${y}`
+  const cachedChunk = await chunks[name]
+  if (cachedChunk) return cachedChunk
 
-  return fetch(`/paint_chunks/${name}`, {
-    method: 'POST',
-    body: await image.convertToBlob(),
-  })
+  try {
+    return await (chunks[name] = (async () => new Uint8ClampedArray(await socket.emitWithAck('readChunk', x + '', y + '')))())
+  } catch {
+    return chunks[name] = (async () => new Uint8ClampedArray(chunkByteLength))()
+  }
+}
+
+async function writeChunk<X extends bigint, Y extends bigint>(x: X, y: Y, data: Uint8ClampedArray, isErase: boolean) {
+  if (data.length != chunkByteLength) throw new RangeError(`Chunk size is not matching (Expected ${chunkByteLength}, Received ${data.length})`)
+
+  const name: ChunkName<X, Y> = `${x},${y}`
+  chunks[name] = (async () => {
+    if (isErase) chunkContext.globalCompositeOperation = 'destination-out'
+    putImageContext.putImageData(new ImageData(data, CHUNK_SIZE), 0, 0)
+    chunkContext.drawImage(putImageCanvas, 0, 0)
+    chunkContext.globalCompositeOperation = 'source-over'
+    return chunkContext.getImageData(0, 0, chunkCanvas.width, chunkCanvas.height).data
+  })()
+
+  const lastModified = lastModifiedTable[name] = performance.now()
+  data = new Uint8ClampedArray(await socket.emitWithAck('writeChunk', x + '', y + '', data, isErase))
+  if (lastModified != lastModifiedTable[name]) return
+  chunks[name] = (async () => data)()
+  putImageContext.putImageData(new ImageData(data, CHUNK_SIZE), 0, 0)
+  context.clearRect(Number(x) * CHUNK_SIZE, Number(y) * CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE)
+  context.drawImage(putImageCanvas, Number(x) * CHUNK_SIZE, Number(y) * CHUNK_SIZE)
 }
 
 function drawLine(x1: number, y1: number, x2: number, y2: number, lineWidth: number, color: CanvasFillStrokeStyles['strokeStyle'], isErase = false) {
@@ -267,11 +277,11 @@ function drawLine(x1: number, y1: number, x2: number, y2: number, lineWidth: num
     const { data } = imgData
     for (let i = 3; i < data.length; i += 4) data[i] = data[i] < 96 ? 0 : 255
 
-    const chunk = readChunk(BigInt(chunkX), BigInt(chunkY))
-    if (!chunk.complete) return
+    const chunk = await readChunk(BigInt(chunkX), BigInt(chunkY))
 
+    putImageContext.putImageData(new ImageData(chunk, CHUNK_SIZE), 0, 0)
     chunkContext.clearRect(0, 0, chunkCanvas.width, chunkCanvas.height)
-    chunkContext.drawImage(chunk, 0, 0)
+    chunkContext.drawImage(putImageCanvas, 0, 0)
 
     if (isErase) chunkContext.globalCompositeOperation = 'destination-out'
     putImageContext.putImageData(imgData, 0, 0)
@@ -281,7 +291,7 @@ function drawLine(x1: number, y1: number, x2: number, y2: number, lineWidth: num
     context.clearRect(chunkX * CHUNK_SIZE, chunkY * CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE)
     context.drawImage(chunkCanvas, chunkX * CHUNK_SIZE, chunkY * CHUNK_SIZE)
 
-    await writeChunk(BigInt(chunkX), BigInt(chunkY), chunkCanvas)
+    await writeChunk(BigInt(chunkX), BigInt(chunkY), putImageContext.getImageData(0, 0, putImageCanvas.width, putImageCanvas.height).data, isErase)
   })())
   return Promise.all(promises)
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,34 +1,36 @@
 import path from 'path'
 import fs from 'fs/promises'
 import express from 'express'
-import { Server } from 'socket.io'
+import EventEmitter from 'events'
 import { createServer } from 'http'
-// import { chunkByteLength } from '../const'
+import { Server, type Socket } from 'socket.io'
+import { chunkByteLength } from '../const'
 
 const app = express()
 app.disable('x-powered-by')
 
 app.use(express.static('public'))
-app.use(express.raw({ limit: Infinity }))
-
-app.get('/paint_chunks/:name', (_req, res) => res.sendFile('blank.png', { root: process.cwd() }))
-app.post('/paint_chunks/:x.:y.png', async (req, res) => {
-  await writeChunk(BigInt(req.params.x), BigInt(req.params.y), req.body)
-  res.end()
-})
 app.use((_req, res) => res.redirect('/'))
 
 export interface Client2Server {
+  ping(): void
   setRange(x1: string, y1: string, x2: string, y2: string): void
-  // chunk(x: string, y: string, data: Uint8ClampedArray): void
+  readChunk(x: string, y: string, callback: (data: Uint8ClampedArray) => void): void
+  writeChunk(x: string, y: string, data: Uint8ClampedArray, isErase: boolean, callback: (data: Uint8ClampedArray) => void): void
 }
 
 export interface Server2Client {
-  chunk(x: string, y: string): void
+  chunk(x: string, y: string, data: Uint8ClampedArray): void
 }
 
 const server = createServer(app)
 const io = new Server<Client2Server, Server2Client>(server.listen(3001, () => console.log(server.address())))
+
+interface ChunkEventMap {
+  chunk: [sender: Socket<Client2Server, Server2Client>, x: string, y: string, data: Uint8ClampedArray]
+}
+
+const emitter = new EventEmitter<ChunkEventMap>
 
 io.on('connection', socket => {
   const range = {
@@ -37,46 +39,84 @@ io.on('connection', socket => {
     x2: 0n,
     y2: 0n,
   }
+  const dirtyChunks: Record<ChunkName, Uint8ClampedArray> = Object.create(null)
   socket.on('setRange', (x1, y1, x2, y2) => {
     try {
       range.x1 = BigInt(x1)
       range.y1 = BigInt(y1)
       range.x2 = BigInt(x2)
       range.y2 = BigInt(y2)
+      for (const key in dirtyChunks) {
+        const [ x, y ] = key.split(',').map(BigInt)
+        if (x < range.x1 || range.x2 < x || y < range.y1 || range.y2 < y) continue
+        socket.emit('chunk', x + '', y + '', dirtyChunks[key as ChunkName])
+        delete dirtyChunks[key as ChunkName]
+      }
     } catch {}
   })
-  // socket.on('chunk', async (x, y, data) => {
-  //   socket.broadcast.emit('chunk', x, y)
-  //   await writeChunk(BigInt(x), BigInt(y), data)
-  // })
+
+  socket.on('readChunk', async (x, y, callback) => {
+    try {
+      callback(await readChunk(BigInt(x), BigInt(y)))
+    } catch {
+      callback(new Uint8ClampedArray(chunkByteLength))
+    }
+  })
+  socket.on('writeChunk', async (x, y, data, isErase, callback) => {
+    if (data.length != chunkByteLength) throw new RangeError(`Chunk size is not matching (Expected ${chunkByteLength}, Received ${data.length})`)
+
+    callback(data = await (async () => {
+      const chunk = await readChunk(BigInt(x), BigInt(y))
+      const imgData = new Uint8ClampedArray(data)
+      if (isErase) for (let i = 0; i < imgData.length; i += 4) {
+        imgData[i + 0] = chunk[i + 0]
+        imgData[i + 1] = chunk[i + 1]
+        imgData[i + 2] = chunk[i + 2]
+        imgData[i + 3] = chunk[i + 3] * (1 - imgData[i + 3])
+      } else for (let i = 0; i < imgData.length; i += 4) {
+        imgData[i + 0] = (1 - imgData[i + 3]) * chunk[i + 0] + imgData[i + 3] * imgData[i + 0]
+        imgData[i + 1] = (1 - imgData[i + 3]) * chunk[i + 1] + imgData[i + 3] * imgData[i + 1]
+        imgData[i + 2] = (1 - imgData[i + 3]) * chunk[i + 2] + imgData[i + 3] * imgData[i + 2]
+        imgData[i + 3] = (1 - (1 - chunk[i + 3] / 255) * (1 - imgData[i + 3] / 255)) * 255
+      }
+      return imgData
+    })())
+    emitter.emit('chunk', socket, x, y, data)
+    await writeChunk(BigInt(x), BigInt(y), data)
+  })
+
+  function updateChunk(sender: Socket<Client2Server, Server2Client>, strX: string, strY: string, data: Uint8ClampedArray) {
+    const x = BigInt(strX), y = BigInt(strY)
+    if (sender == socket) return
+    if (x < range.x1 || range.x2 < x || y < range.y1 || range.y2 < y) {
+      dirtyChunks[`${x},${y}`] = data
+      return
+    }
+    socket.emit('chunk', strX, strY, data)
+  }
+  emitter.on('chunk', updateChunk)
+  socket.on('disconnecting', () => emitter.off('chunk', updateChunk))
 })
 
-export type ChunkName<X extends bigint = bigint, Y extends bigint = bigint> = `${X}.${Y}.png`
+export type ChunkName<X extends bigint = bigint, Y extends bigint = bigint> = `${X},${Y}`
 
-// const chunks: Record<ChunkName, Promise<Uint8ClampedArray>> = {}
+const chunks: Record<ChunkName, Promise<Uint8ClampedArray>> = Object.create(null)
 
-// async function readChunk<X extends bigint, Y extends bigint>(x: X, y: Y) {
-//   const name: ChunkName<X, Y> = `${x},${y}.png`
-//   const cachedChunk = await chunks[name]
-//   if (cachedChunk) return cachedChunk
+async function readChunk<X extends bigint, Y extends bigint>(x: X, y: Y) {
+  const name: ChunkName<X, Y> = `${x},${y}`
+  const cachedChunk = await chunks[name]
+  if (cachedChunk) return cachedChunk
 
-//   try {
-//     return await (chunks[name] = (async () => new Uint8ClampedArray(await fs.readFile(path.join('paint_chunks', name))))())
-//   } catch {
-//     return chunks[name] = (async () => new Uint8ClampedArray(chunkByteLength))()
-//   }
-// }
+  try {
+    return await (chunks[name] = (async () => new Uint8ClampedArray(await fs.readFile(path.join('paint_chunks', name))))())
+  } catch {
+    return chunks[name] = (async () => new Uint8ClampedArray(chunkByteLength))()
+  }
+}
 
-// async function writeChunk<X extends bigint, Y extends bigint>(x: X, y: Y, data: Uint8ClampedArray) {
-//   if (data.length != chunkByteLength) throw new RangeError(`Chunk size is not matching (Expected ${chunkByteLength}, Received ${data.length})`)
+async function writeChunk<X extends bigint, Y extends bigint>(x: X, y: Y, data: Uint8ClampedArray) {
+  const name: ChunkName<X, Y> = `${x},${y}`
+  chunks[name] = (async () => data)()
 
-//   const name: ChunkName<X, Y> = `${x},${y}.png`
-//   chunks[name] = (async () => data)()
-
-//   await fs.writeFile(path.join('public/paint_chunks', name), data)
-// }
-
-async function writeChunk<X extends bigint, Y extends bigint>(x: X, y: Y, data: string | import('stream') | NodeJS.ArrayBufferView<ArrayBufferLike> | Iterable<string | NodeJS.ArrayBufferView<ArrayBufferLike>> | AsyncIterable<string | NodeJS.ArrayBufferView<ArrayBufferLike>>) {
-  const name: ChunkName<X, Y> = `${x}.${y}.png`
-  await fs.writeFile(path.join('public/paint_chunks', name), data)
+  await fs.writeFile(path.join('paint_chunks', name), await chunks[name])
 }
